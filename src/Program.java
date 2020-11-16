572f13cb8180c922ee3d263bd3b0e106983d5845
luciano-developer <lcoliveiraa@gmail.com>
2020-11-16 16:18:47 -0300

    aula 2 file (fileWriter e BufferedWriter)

--- src/Program.java
+++ src/Program.java
@@ -1,25 +1,21 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		String path = "C:\\Users\\minha\\workspace-java-completo\\i	n.txt";
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-		  
-			String line = br.readLine();
-			
-			while (line!= null) {
-				System.out.println(line);
-				line = br.readLine();
-				
+
+		String[] lines = new String[] { "primeira linha", "segunda linha" };
+       
+		String path = "C:\\Users\\minha\\workspace-java-completo\\out.txt";
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
-					
 		} catch (IOException e) {
-			System.out.println("Erro:"+e.getMessage());
+			// TODO: handle exception
 		}
 	}
 
